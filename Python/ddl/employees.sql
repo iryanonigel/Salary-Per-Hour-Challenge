f28279ff87b7203eec64f9e6633c787e54b0b93a
Iryano Nigel <iryanonigell@gmail.com>
2023-06-25 16:55:59 +0700

    Delete Python directory

--- Python/ddl/employees.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE di.employees ( -- correspond with destination table (schema and table name)
-	employe_id int,
-	branch_id int,
-	salary int,
-	join_date date,
-	resign_date date,
-	ingesttime timestamp,
-	ingest_from text
-);
